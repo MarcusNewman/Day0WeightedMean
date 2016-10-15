f5092b2c6d8cf1253315b849675432a10c3e4004
Marcus Newman <marcusgeorgenewman@gmail.com>
2016-10-15 13:58:34 -0700

    Completed

---
+++ Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        Console.ReadLine();
+        var X = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+        var W = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+        Console.WriteLine(WeightedMean(X, W));
+    }
+
+    public static string WeightedMean(int[] values, int[] weights)
+    {
+        var sumOfWeightsTimesValues = 0;
+        for(var i = 0; i < values.Length; i++)
+        {
+            sumOfWeightsTimesValues += values[i] * weights[i];
+        }
+        var sumOfWeights = 0;
+        foreach (var weight in weights) sumOfWeights += weight;
+        var result =(decimal)sumOfWeightsTimesValues / sumOfWeights;
+        return String.Format("{0:0.0}", result);
+    }
+}
---
+++ Tests/SolutionUnitTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReflectionUnitTesting;
+using System.Reflection;
+using System.Collections.Generic;
+
+[TestClass]
+public class SolutionUnitTests
+{
+    [TestMethod]
+    public void AssemblyShouldExist()
+    {
+        GetAssembly();
+    }
+
+    private static Assembly GetAssembly()
+    {
+        return ReflectionAssert.AssemblyExists("Day0WeightedMean.exe");
+    }
+
+    [TestMethod]
+    public void ClassShouldExist()
+    {
+        GetClass();
+    }
+
+    private static Type GetClass()
+    {
+        return GetAssembly().TypeExists("Solution");
+    }
+
+    [TestMethod]
+    public void MethodShouldExist()
+    {
+        GetMethod();
+    }
+
+    private static MethodInfo GetMethod()
+    {
+        return GetClass().MethodExists(
+            "WeightedMean",
+            shouldBeStatic: true,
+            shouldReturnType: typeof(string),
+            parameterTypesAndNames: new List<Tuple<Type, string>> {
+                Tuple.Create(typeof(int[]), "values"),
+                Tuple.Create(typeof(int[]), "weights") });
+    }
+
+    //Sample Input
+    //5
+    //10 40 30 50 20
+    //1 2 3 4 5
+    //Sample Output
+    //32.0
+    [TestMethod]
+    public void WeightedMean104030502012345ShouldBe320()
+    {
+        var parameters = new object[] { new int[] { 10, 40, 30, 50, 20 }, new int[] { 1, 2, 3, 4, 5 } };
+        var expectedResult = "32.0";
+        GetMethod().TestMethod(null, parameters, expectedResult);
+    }
+}
+
